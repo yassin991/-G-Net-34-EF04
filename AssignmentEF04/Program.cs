3878a23be1ac840a286336bb47ebf24ea60c702b
yassin991 <yassenelsherbiny69@gmail.com>
2026-04-22 16:34:18 +0200

    Add project files.

---
+++ AssignmentEF04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentEF04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
